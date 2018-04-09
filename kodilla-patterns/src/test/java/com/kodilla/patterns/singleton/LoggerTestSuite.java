3ce84eb502d75ede27ae3d2a50936b7762318198
Lucyna.Bluszcz <lucynabluszcz@gmail.com>
2018-04-10 00:17:28 +0200

    11_1

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -8,7 +8,7 @@ public class LoggerTestSuite {
     private static Logger logger;
 
     @Test
-    public void testGetLastLog(){
+    public void testGetLastLog() {
         //Given
         Logger.getInstance().log("Example log");
         //When
@@ -17,6 +17,16 @@ public class LoggerTestSuite {
         Assert.assertEquals("Example log", lastLog);
     }
 
+    @Test
+    public void testGetSecondLog() {
+        //Given
+        Logger.getInstance().log("Example log");
+        Logger.getInstance().log("Second example log");
 
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("Second example log", lastLog);
+    }
 
 }
